50cd0d5b7af3aeaf0d2f18c2daf668f53d03d922
Johann W Billar <jwbillar@gmail.com>
2015-10-06 22:02:10 +0000

    Added validation to model for address and description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 50, :minimum => 3 }
+	validates :address, :presence => true, :length => { :maximum => 300, :minimum => 10}
+	validates :description, :presence => true, :length => { :maximum => 300, :minimum => 10 }  
 end
