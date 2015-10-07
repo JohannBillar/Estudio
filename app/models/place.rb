8487f5a4aca07c7141ae29eb4adfed1c09d7c07e
Johann W Billar <jwbillar@gmail.com>
2015-10-07 16:56:01 +0000

    Set-up Map Integration - Convert address to lat long

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
 	validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 50, :minimum => 3 }
 	validates :address, :presence => true, :length => { :maximum => 300, :minimum => 10}
 	validates :description, :presence => true, :length => { :maximum => 300, :minimum => 10 }  
