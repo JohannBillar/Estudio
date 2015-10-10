abc22c5759d9b12915105f346e03884698372324
Johann W Billar <jwbillar@gmail.com>
2015-10-10 21:25:19 +0000

    Hooked up the Add a Photo button on the place detail page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
+
 	def create
 		@place = Place.find(params[:place_id])
-		@comment = @place.comments.create(comments_params.merge(:user => current_user))
+		@comment = @place.comments.create(comment_params.merge(:user => current_user))
 		if @comment.valid?
 			redirect_to place_path(@place)
 		else
@@ -12,7 +13,7 @@ class CommentsController < ApplicationController
 
 	private
 
-	def comments_params
+	def comment_params
 		params.require(:comment).permit(:message, :rating)
 	end
 end
