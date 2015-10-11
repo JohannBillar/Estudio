79d482ac34ff458dce8316d853046cb296fbc10e
Johann W Billar <jwbillar@gmail.com>
2015-10-11 00:48:08 +0000

    Created a bootstrap modal for teh Add a photo feature

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,8 @@ class Photo < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
 
+	validates :caption, :presence => true  
+	validates :picture, :presence => true
+
 	mount_uploader :picture, PictureUploader
 end
