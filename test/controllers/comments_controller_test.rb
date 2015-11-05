3c232f827a6acdc35dc4095ecadd14ae126f61d8
JohannBillar <jwbillar@gmail.com>
2015-11-05 04:58:58 +0000

    Added 2 more assertions to comments controller test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,7 +5,7 @@ class CommentsControllerTest < ActionController::TestCase
 		user = FactoryGirl.create(:user)
 		sign_in user
 		place = FactoryGirl.create(:place)
-		assert_difference 'Comment.count' do
+		assert_difference 'Comment.count', +1 do
 			post :create, :place_id => place.id, :comment => {
 				:message => 'Howdie',
 				:rating => 'one star'
@@ -13,5 +13,10 @@ class CommentsControllerTest < ActionController::TestCase
 		end
 		assert_redirected_to place_path(place)
 		assert_equal 1, user.comments.count
+
+		comment = user.comments.first
+		assert_equal 'Howdie', comment.message
+		assert_equal 'one star', comment.rating
+
 	end
 end
