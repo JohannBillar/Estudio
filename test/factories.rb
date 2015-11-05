426cdd90a8477e722a863e5c41f28ffa06aa7de5
JohannBillar <jwbillar@gmail.com>
2015-11-05 03:07:50 +0000

    Comment controller test added

--- test/factories.rb
+++ test/factories.rb
@@ -1,14 +1,17 @@
 FactoryGirl.define do
+	
 	factory :user do
 		sequence :email do |n| 
 			"jwbillar#{n}@gmail.com"
-	end
+		end
 		password "Rallib1975"
 	end
+
 	factory :place do
 		name "My Music Store"
 		description "Only store around"
 		address "5000 Newport Ave, San Diego, CA 92107"
 		association :user
 	end
+
 end
